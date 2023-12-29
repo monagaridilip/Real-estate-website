82a4ef66f9f740005b65099c5675968cfa664ca0
monagaridilip <monagaridilip07@gmail.com>
2023-12-29 17:20:04 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -33,16 +33,21 @@ export const signup = async (req,res,next) =>{
 
 export const signin = async(req,res,next)=>{
     const {email,password} = req.body;
-    const validUser = await User.findOne({email})
-    if(!validUser){
-        return next(errorHandler(404,'User Not Found'))
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'User Not Found'))
+        }
+        const validPassword = bcrypt.compareSync(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(401,'Invalid credentials'))
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET_KEY)
+        const {password:pass,...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
+        
+    } catch (error) {
+        next(error)
     }
-    const validPassword = bcrypt.compareSync(password,validUser.password)
-    if(!validPassword){
-        return next(errorHandler(401,'Invalid credentials'))
-    }
-    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET_KEY)
-    const {password:pass,...rest} = validUser._doc;
-    res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
     
 }
