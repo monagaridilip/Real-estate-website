267ee74aa0fe111232fd8267a05c1e199120bac1
monagaridilip <monagaridilip07@gmail.com>
2023-12-29 15:28:04 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import {errorHandler} from '../utils/error.js'
 export const signup = async (req,res,next) =>{
     const {username, email, password} = req.body;
     
@@ -9,7 +11,7 @@ export const signup = async (req,res,next) =>{
         }
         let user = await User.findOne({email})
         let user2 = await User.findOne({username})
-        if(user ){
+        if(user){
             return res.json({err:`${email} is already exits`})
         }
         if(user2){
@@ -27,4 +29,20 @@ export const signup = async (req,res,next) =>{
     } catch (error) {
         next(error);
     }
+}
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    const validUser = await User.findOne({email})
+    if(!validUser){
+        return next(errorHandler(404,'User Not Found'))
+    }
+    const validPassword = bcrypt.compareSync(password,validUser.password)
+    if(!validPassword){
+        return next(errorHandler(401,'Invalid credentials'))
+    }
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET_KEY)
+    const {password:pass,...rest} = validUser._doc;
+    res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
+    
 }
