bc4995a27ebc03e193ae07d45a2d20489044275b
monagaridilip <monagaridilip07@gmail.com>
2024-01-23 00:40:57 +0530

    complete update user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,75 +1,60 @@
-import User from '../models/user.model.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import {errorHandler} from '../utils/error.js'
-export const signup = async (req,res,next) =>{
-    const {username, email, password} = req.body;
-    
-    try {
-        if(!username | !email | !password){
-            return res.status(400).json({err:"enter all the fields"})
-        }
-        let user = await User.findOne({email})
-        let user2 = await User.findOne({username})
-        if(user){
-            return res.json({err:`${email} is already exits`})
-        }
-        if(user2){
-            return res.json({err:`${username} is already exits`})
-        }
-        // console.log(user)
-        let hashedPassword = bcrypt.hashSync(password,10)
-        
-        user = await User.create({username,email,password:hashedPassword})
-        // if(!user){
-        //     return res.status(404).json({err:"user not created"})
-        // }
-        res.status(201).json({user:"user created successfully"})
-        
-    } catch (error) {
-        next(error);
-    }
-}
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-export const signin = async(req,res,next)=>{
-    const {email,password} = req.body;
-    try {
-        const validUser = await User.findOne({email})
-        if(!validUser){
-            return next(errorHandler(404,'User Not Found'))
-        }
-        const validPassword = bcrypt.compareSync(password,validUser.password)
-        if(!validPassword){
-            return next(errorHandler(401,'Invalid credentials'))
-        }
-        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET_KEY)
-        const {password:pass,...rest} = validUser._doc;
-        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
-        
-    } catch (error) {
-        next(error)
-    }
-    
-}
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const google = async (req,res,next)=>{
-    const {name,email,photoURL} = req.body;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
     try {
-        const user = await User.findOne({email})
-        if(user){
-            const token = jwt.sign({id:user.id},process.env.JWT_SECRET_KEY)
-            const {password:pass,...rest} = user._doc;
-            res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
-        }else{
-            const generatePassword = Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
-            const hashedPassword = bcrypt.hashSync(generatePassword,10);
-            const newUser = await User.create({username:name.split(' ').join('').toLowerCase() +
-            Math.random().toString(36).slice(-4),email:email,password:hashedPassword,avatar:photoURL});
-            const token = jwt.sign({id:newUser.id},process.env.JWT_SECRET_KEY)
-            const {password:pass,...rest} = newUser._doc;
-            res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
-        }
+      const user = await User.findOne({ email: req.body.email })
+      if (user) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+        const { password: pass, ...rest } = user._doc;
+        res
+          .cookie('access_token', token, { httpOnly: true })
+          .status(200)
+          .json(rest);
+  
+      } else {
+        const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+        const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+        const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4) , email: req.body.email, password: hashedPassword, avatar: req.body.photo });
+        await newUser.save();
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY);
+        const { password: pass, ...rest } = newUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+  
+      }
     } catch (error) {
-        next(error)
+      next(error)
     }
-}
+  }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,23 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useRef, useState } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase'
+import { updateUserStart,updateUserSuccess,updateUserFailure } from "../redux store/user/userSlice";
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
-  const fileRef = useRef();
-  const [file,setFile] = useState(undefined)
-  const [filePer,setFilePerc] = useState(0);
-  const [fileUploadError,setFileUploadError] = useState(false);
-  const [formData,setFormData] = useState({});
-  
-  console.log(filePer)
-  console.log(formData)
-  console.log(fileUploadError)
-  useEffect(()=>{
-    if(file){
+  const fileRef = useRef(null);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [fileperc, setfileperc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (file) {
       handleFileUpload(file);
     }
-  },[file]);
+  }, [file]);
 
   const handleFileUpload = (file) => {
     const storage = getStorage(app);
@@ -30,7 +30,7 @@ export default function Profile() {
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setFilePerc(Math.round(progress));
+        setfileperc(Math.round(progress));
       },
       (error) => {
         setFileUploadError(true);
@@ -42,34 +42,77 @@ export default function Profile() {
       }
     );
   };
+  
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data)
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.errorMessage));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-3'>
+      <form className='flex flex-col gap-3' onSubmit={handleSubmit}>
          <input type="file" 
          ref={fileRef} 
          hidden 
          accept="image/*"
          onChange={(e)=>setFile(e.target.files[0])}
          />
-        <img onClick={()=>fileRef.current.click()} src={ formData.avatar || currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"/>
-         <p className="text-sm self-center">
+       <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className="text-sm self-center">
          {
            fileUploadError ? (<span className="text-red-700">Error Uploading image(Image must be less than 2mb)</span>)
-           : filePer >0 && filePer < 100 ? <span className="text-black-700">Uploading {filePer}% completed</span>
-           : filePer == 100 ? <span className="text-green-700">Image Uploaded Successfully</span>
+           : fileperc >0 && fileperc < 100 ? <span className="text-black-700">Uploading {fileperc}% completed</span>
+           : fileperc == 100 ? <span className="text-green-700">Image Uploaded Successfully</span>
            : ''
           }
           </p>
-        <input type="text" name="username" id="username" placeholder="username" className="border p-3 rounded-lg"/>
-        <input type="email" name="email" id="email" placeholder="email" className="border p-3 rounded-lg"/>
-        <input type="password" name="password" id="password" placeholder="password" className="border p-3 rounded-lg"/>
-        <button className="bg-slate-700 rounded-lg text-white p-3 uppercase hover:opacity-90">Update</button>
+        <input type="text" defaultValue={currentUser.username} onChange={handleChange} name="username" id="username" placeholder="username" className="border p-3 rounded-lg"/>
+        <input type="email" defaultValue={currentUser.email} onChange={handleChange} name="email" id="email" placeholder="email" className="border p-3 rounded-lg"/>
+        <input type="password" name="password" id="password" onChange={handleChange} placeholder="password" className="border p-3 rounded-lg"/>
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Update'}
+        </button>
       </form>
       <div className="flex justify-between mt-4">
         <span className="text-red-600 cursor-pointer ">Delect account</span>
         <span className="text-red-600 cursor-pointer ">Sign out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
     </div>
   )
 }
