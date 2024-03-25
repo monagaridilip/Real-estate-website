ac20a84eebe60870f8e98b6817de7286150de962
monagaridilip <monagaridilip07@gmail.com>
2024-03-25 19:23:01 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20  max-w-6xl w-full mx-auto'>
+      <h1 className='font-bold text-3xl mb-4 text-slate-800'>About Reat Estate</h1>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ex illo iusto magni, debitis similique voluptate ipsa obcaecati molestiae vitae cupiditate rem error suscipit eaque ea sint dolor quibusdam cum cumque fuga nesciunt distinctio molestias ducimus. Unde culpa nesciunt quidem ab necessitatibus dignissimos quibusdam. Asperiores laudantium minus corporis numquam incidunt!</p>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ex illo iusto magni, debitis similique voluptate ipsa obcaecati molestiae vitae cupiditate rem error suscipit eaque ea sint dolor quibusdam cum cumque fuga nesciunt distinctio molestias ducimus. Unde culpa nesciunt quidem ab necessitatibus dignissimos quibusdam. Asperiores laudantium minus corporis numquam incidunt!</p>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ex illo iusto magni, debitis similique voluptate ipsa obcaecati molestiae vitae cupiditate rem error suscipit eaque ea sint dolor quibusdam cum cumque fuga nesciunt distinctio molestias ducimus. Unde culpa nesciunt quidem ab necessitatibus dignissimos quibusdam. Asperiores laudantium minus corporis numquam incidunt!</p>
     </div>
   )
 }
